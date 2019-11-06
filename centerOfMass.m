059162e88dc608b47a0e32cee00074cb9ad7b709
Noga Larry <noga.larry@mail.huji.ac.il>
2019-11-06 14:46:20 +0200

    initial comiit of centerOfMass

---
+++ centerOfMass.m
@@ -0,0 +1,16 @@
+function [PD,ind_PD] = centerOfMass (TC, directions)
+% This function finds the PD of a cll given its tuning curve and the
+% experimantal directions using the center of mass.
+% Inputs:    TC         The tuning curce of the cell
+%            directions A vector of the directions in the same order as TC. 
+% Outputs:   PD         The nearest direction to the preferred direction of
+%                       the cell as calculated by the center of mass.
+% Outputs:   ind_PD         The nearest direction to the preferred direction of
+%                       the cell as calculated by the center of mass.
+
+v_x = cosd(directions)*TC;
+v_y = sind(directions)*TC;
+pd_exact = mod(atan2d(v_y,v_x),360);
+dist = abs(directions-pd_exact);
+[~,ind_PD] = min(dist);
+PD = directions(ind_PD);
