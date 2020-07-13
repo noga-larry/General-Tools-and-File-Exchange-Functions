1b4c6519027f989e8975e332e36e2ea8d15ada3c
nogalarry <noga.larry@mail.huji.ac.il>
2020-07-13 17:21:59 +0300

    Add downSampleToBinns

---
+++ downSampleToBins.m
@@ -0,0 +1,9 @@
+function binnedTrace = downSampleToBins(trace,binSz)
+
+trace = trace';
+totalTimeWithRem = size(trace,1);
+numTrials = size(trace,2);
+trace = trace(1:(totalTimeWithRem-rem(totalTimeWithRem,binSz)),:);
+totalTime = size(trace,1);
+binnedTrace = reshape(trace,binSz,totalTime/binSz,numTrials);
+binnedTrace = squeeze(sum(binnedTrace))';
