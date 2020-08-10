ca520ad889fac371dcf8b6781da39d1383a46596
nogalarry <noga.larry@mail.huji.ac.il>
2020-08-10 14:59:54 +0300

    changing downsample fuction. BUG!!! Need to be fixed

--- downSampleToBins.m
+++ downSampleToBins.m
@@ -6,4 +6,4 @@ numTrials = size(trace,2);
 trace = trace(1:(totalTimeWithRem-rem(totalTimeWithRem,binSz)),:);
 totalTime = size(trace,1);
 binnedTrace = reshape(trace,binSz,totalTime/binSz,numTrials);
-binnedTrace = squeeze(sum(binnedTrace))';
+binnedTrace = squeeze(sum(binnedTrace,1))';
