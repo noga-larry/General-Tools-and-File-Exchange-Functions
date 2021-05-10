63f1490404570d5401461a49fc88a34066e4351c
nogalarry <noga.larry@mail.huji.ac.il>
2021-05-10 13:10:27 +0300

    plotHistForPC: INCOMPLETE!!!!

--- plotHistForFC.m
+++ plotHistForFC.m
@@ -1,8 +1,28 @@
-function [counts,centers] = plotHistForFC(trace,bins,varargin)
+function [counts,centers] = plotHistForFC(trace,centers,varargin)
 
-[counts,centers] = hist(trace, bins);
+
+isCDF = false;
+if any(strcmp(varargin,'cdf'))
+    isCDF = true;
+    varargin = setdiff(varargin, 'cdf');
+end
+
+isNormalized = true;
+if any(strcmp(varargin,'unNormalized'))
+    isNormalized = false;
+    varargin = setdiff(varargin, 'unNormalized');
+end
+
+[counts, centers] = hist(trace, centers);
 %centers = 0.5*(edges(1:end-1)+edges(2:end));
-counts = counts/sum(counts);
+if isNormalized
+    counts = counts/sum(counts);
+end
+if isCDF
+    counts = cumsum(counts,'reverse');
+end
 plot(centers, counts,varargin{:});
+
+
 
 
