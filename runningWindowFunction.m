24f7016d7e3a324ad45e8060cadc4c870230b17b
nogalarry <noga.larry@mail.huji.ac.il>
2021-08-25 14:27:07 +0300

    runningWindowFunction: add option to determine putput size

--- runningWindowFunction.m
+++ runningWindowFunction.m
@@ -1,14 +1,18 @@
 function returnTrace = ...
-    runningWindowFunction(trace,func,windowSize)
+    runningWindowFunction(trace,func,windowSize, dimReturn)
 
+if ~exist('dimReturn','var')
+    dimReturn = 1;
+end
+    
 edgeSize = ceil(windowSize/2);
 runningWindow = -edgeSize:edgeSize;
 centers = (edgeSize+1):(size(trace,1)-edgeSize);
-returnTrace = nan(length(centers),1);
+returnTrace = nan(length(centers),dimReturn);
 
 for i = 1:length(centers)
     slicedTrace = trace(centers(i)+runningWindow,:);
-    returnTrace(i) = func(slicedTrace);
+    returnTrace(i,:) = func(slicedTrace);
 end
     
 
