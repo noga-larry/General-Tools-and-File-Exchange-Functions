b461b0db5bc3feb102a6ddf75fc4c5d7d160dcd1
nogalarry <noga.larry@mail.huji.ac.il>
2021-10-17 10:58:32 +0300

    Documentation

--- times2Binary.m
+++ times2Binary.m
@@ -1,5 +1,18 @@
 function binary = times2Binary(eventTimes,alignmemtTime,window)
 
+% This function converts time stamps to binary (raster) vectors. 
+% Inputs: 
+%   eventTimes -    The time of the events (relaive to the trial onset or
+%                   the onset of data collection - in the case of extended 
+%                   data)
+%   alignmemtTime - The time of the event within the trial to which to 
+%                   align the event to (relative to the same onset as 
+%                   eventTimes) 
+%   window -        The window of time to create around the event
+% Output:
+%   binary -        A binary vector, the raster of the events from 
+%                   eventTimes in the window around alignmemtTime.
+
 eventTimes = eventTimes - alignmemtTime;
 eventInds = ceil(eventTimes);
 eventInds = intersect(eventInds,window);
