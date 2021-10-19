559ee996c6d2d28fb81fdf1a6473ad1ef7b73865
nogalarry <noga.larry@mail.huji.ac.il>
2021-10-19 10:19:01 +0300

    Add alignTimesFactory

---
+++ alignmentTimesFactory.m
@@ -0,0 +1,40 @@
+function alignmentTimes = alignmentTimesFactory(data,ind,alignTo)
+
+alignmentTimes = nan(1,length(ind));
+
+
+switch alignTo
+    
+    case 'cue'
+        for t=1:length(ind)
+            alignmentTimes(t) = data.trials(ind(t)).cue_onset;
+        end
+        
+    case 'targetMovementOnset'
+        for t=1:length(ind)
+            alignmentTimes(t) = data.trials(ind(t)).movement_onset;
+        end
+        
+    case 'reward'
+        for t=1:length(ind)
+            if ~ isnan(data.trials(ind(t)).rwd_time_in_extended)
+                alignmentTimes(t) = data.trials(ind(t)).rwd_time_in_extended;
+            else
+                alignmentTimes(t) = nan;
+            end
+        end 
+        
+    case  'pursuitLatency'
+        
+        % plave nans in filed trials
+        bool_fail = [data.trials(ind).fail];        
+        [~, latency_tmp, ~, init_time ,~]  =....
+            fitTimingDirectionAndGain(data,ind(~bool_fail));
+        latency = nan(1,length(ind));
+        latency(~bool_fail) = latency_tmp;
+        for t=1:length(ind)
+             alignmentTimes(t) = data.trials(ind(t)).movement_onset +...
+                 latency(t);
+        end
+end
+end
