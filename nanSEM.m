546c94105efc0c50e4e9bd8215a58c0471c71cd2
nogalarry <noga.larry@mail.huji.ac.il>
2022-06-02 17:52:56 +0300

    correct for row as well as column vecs

--- nanSEM.m
+++ nanSEM.m
@@ -1,7 +1,13 @@
 function SEM = nanSEM(M,dim)
 
 if ~(exist('dim','var')==1)
-    dim=1;
+    
+    if sum(size(M)>1)==1
+        dim = find(size(M)>1);
+    else
+        dim=1;
+    end
+    
 end
 STD = nanstd(M,1,dim);
 normalization = sqrt(sum(~isnan(M),dim));
